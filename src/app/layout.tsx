860af05db932205a053ad239e8dbde1ab446fa02
Dragan Vulesic <dragan.vulesic@jolausa.com>
2025-12-16 13:58:54 +0100

    Analitics 101 put

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Script from "next/script";
+
 import "../../public/styles/bootstrap.min.css";
 import "../../public/styles/animate.min.css";
 import "animate.css";
@@ -8,126 +10,79 @@ import "swiper/css";
 import "swiper/css/bundle";
 
 import "../../public/styles/app-home-page.css";
-
-// Global Style
 import "../../public/styles/style.css";
 import "../../public/styles/responsive.css";
-// Global RTL Style
 import "../../public/styles/rtl.css";
 
 import type { Metadata } from "next";
 import { Inter, Saira } from "next/font/google";
 import AosAnimation from "@/components/Layouts/AosAnimation";
 import GoTop from "@/components/Layouts/GoTop";
-import Script from "next/script";
 import ClientAnalytics from "@/components/ClientAnalytics/ClientAnalytics";
 
-// For all body text font
 const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   variable: "--font-inter",
-  display: "swap"
+  display: "swap",
 });
 
-// For all heading font
 const saira = Saira({
   subsets: ["latin"],
   variable: "--font-saira",
-  display: "swap"
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "MiDaFin - Zvanična prezentacija ",
-  description: "Zvanična prezentacija knjigovodstvena agencija MiDaFin.doo"
+  description: "Zvanična prezentacija knjigovodstvena agencija MiDaFin.doo",
 };
 
 export default function RootLayout({
-  children
-}: Readonly<{
+  children,
+}: {
   children: React.ReactNode;
-}>) {
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
-  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+}) {
+  const TAG_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID;
+  const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
   return (
-    <html lang="sr">
-      <head />
-      <body className={`${inter.variable} ${saira.variable}`}>
-        {/* Google Tag Manager */}
-        {/* {gtmId && (
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-              `
-            }}
-          />
-        )} */}
-        {/* End Google Tag Manager */}
-
-        {/* Google Tag Manager (noscript) */}
-        {/* {gtmId && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        )} */}
-        {/* End Google Tag Manager (noscript) */}
-
-        {/* GA4 – isto kao na starom projektu */}
-        {/* {gaId && (
+    <html lang="sr" suppressHydrationWarning>
+      <head>
+        {TAG_ID ? (
           <>
             <Script
-              id="ga4-src"
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${TAG_ID}`}
+              strategy="beforeInteractive"
             />
-            <Script id="ga4-init" strategy="afterInteractive">
+            <Script id="gtag-init" strategy="beforeInteractive">
               {`
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+                function gtag(){window.dataLayer.push(arguments);}
+                window.gtag = window.gtag || gtag;
+
                 gtag('js', new Date());
-                gtag('config', '${gaId}', {
-                  send_page_view: false
-                });
+
+                // Google Ads tag (glavni tag)
+                gtag('config', '${TAG_ID}');
+
+                // GA4 (analytics) destination
+                ${
+                  GA_ID
+                    ? `gtag('config', '${GA_ID}', { send_page_view: false });`
+                    : ""
+                }
               `}
             </Script>
           </>
-        )} */}
-        {/* End GA4 */}
+        ) : null}
+      </head>
 
+      <body className={`${inter.variable} ${saira.variable}`}>
         {children}
-
         <AosAnimation />
         <GoTop />
-
-        {/* SPA pageview tracking */}
         <ClientAnalytics />
-
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-          `}
-        </Script>
       </body>
     </html>
   );
